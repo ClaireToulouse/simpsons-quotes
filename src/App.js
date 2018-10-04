9626d9ebc7ffc0c04ff563d08984d6e3f408efc9
Claire Lebastard <claire_lebatard@hotmail.com>
2018-10-04 14:45:41 +0200

    quete state

--- src/App.js
+++ src/App.js
@@ -5,16 +5,30 @@ import "./App.css";
 import Lamp from "./Lamp";
 import Quotes from "./Quotes";
 
-class App extends Component {
+class App extends Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
+      on: false
+    };
+  }
+  handleClick = () => {
+    this.setState({ on: !this.state.on });
+  };
   render() {
+    const atWork = this.state.on ? 'Homer is working !' : 'Homer is NOT working';
+    const spinning = this.state.on ? 'faster' : 'App-logo';
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className={spinning} alt="logo" />
           <h1 className="App-title">Simpsons Quotes</h1>
+          <button 
+          onClick={this.handleClick}
+          className={atWork}>{atWork}</button>
         </header>
         <Lamp on />
-        <Lamp />
+        <Lamp />       
         <Quotes/>
       </div>
     );
